7a2e6a7c2fc64c33311dd1304a1e9e786a936bec
basestations <santosme23@up.edu>
2022-09-27 13:03:06 -0700

    Functional Reset

--- app/src/main/java/com/example/a15squares/GameSurfaceView.java
+++ app/src/main/java/com/example/a15squares/GameSurfaceView.java
@@ -58,81 +58,7 @@ public class GameSurfaceView extends SurfaceView implements View.OnTouchListener
     public GameSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
-
-        //initialization
-        gridSize = 4;
-        emptyValue = gridSize*gridSize;
-        numbers = new int[emptyValue];
-
-        //arbitrary values
-        space = 5;
-        size = 200;
-        initTop = 20;
-        initLeft = 20;
-
-        initBottom = initTop + size;
-        initRight = initLeft + size;
-
-        squares = new Square[gridSize][gridSize];
-
-        //setting colors
-        closedColor = new Paint();
-        closedColor.setColor(Color.WHITE);
-
-        openColor = new Paint();
-        openColor.setColor(Color.DKGRAY);
-
-        correctColor = new Paint();
-        correctColor.setColor(Color.GREEN);
-
-        //color for the numbers/text
-        textColor = new Paint();
-        textColor.setColor(Color.BLACK);
-        textColor.setTextSize(100);
-        textColor.setTextAlign(Paint.Align.CENTER);
-
-        //initialization
-        for(int n : numbers){
-            n = 0;
-        }
-
-        //assigns random numbers to each square
-        int assign;
-        for(int j = 0; j < gridSize; j++) {
-            for(int i = 0; i < gridSize; i++) {
-
-                //making sure number isn't already in use
-                boolean pass;
-
-                do{
-                    pass = true;
-                    assign = rand.nextInt(emptyValue) + 1;
-
-                    for (int n : numbers) {
-                        if (assign == n){
-                            pass = false;
-                        }
-                    }
-                } while (!pass);
-
-                for(int k = 0; k < emptyValue; k++){
-                    if(numbers[k] == 0){
-                        numbers[k] = assign;
-                        break;
-                    }
-                }
-
-                //creates square
-                squares[i][j] = new Square(
-                        space + initLeft + (initRight - initLeft) * i,
-                        space + initTop + (initBottom - initTop) * j,
-                        initRight + (initRight - initLeft) * i,
-                        initBottom + (initBottom - initTop) * j,
-                        assign);
-
-                //Log.d("Testing","init " + squares[i][j].getNum());
-            }
-        }
+        init();
     }
 
     protected void onDraw(Canvas canvas) {
@@ -276,7 +202,88 @@ public class GameSurfaceView extends SurfaceView implements View.OnTouchListener
 
     @Override
     public void onClick(View view) {
+        //this onClick acts as a reset
         Log.d("Testing","click");
 
+        init();
+        invalidate();
+    }
+
+    //initialization method
+    private void init(){
+        //initialization
+        gridSize = 4;
+        emptyValue = gridSize*gridSize;
+        numbers = new int[emptyValue];
+
+        //arbitrary values
+        space = 5;
+        size = 200;
+        initTop = 20;
+        initLeft = 20;
+
+        initBottom = initTop + size;
+        initRight = initLeft + size;
+
+        squares = new Square[gridSize][gridSize];
+
+        //setting colors
+        closedColor = new Paint();
+        closedColor.setColor(Color.WHITE);
+
+        openColor = new Paint();
+        openColor.setColor(Color.DKGRAY);
+
+        correctColor = new Paint();
+        correctColor.setColor(Color.GREEN);
+
+        //color for the numbers/text
+        textColor = new Paint();
+        textColor.setColor(Color.BLACK);
+        textColor.setTextSize(100);
+        textColor.setTextAlign(Paint.Align.CENTER);
+
+        //initialization
+        for(int n : numbers){
+            n = 0;
+        }
+
+        //assigns random numbers to each square
+        int assign;
+        for(int j = 0; j < gridSize; j++) {
+            for(int i = 0; i < gridSize; i++) {
+
+                //making sure number isn't already in use
+                boolean pass;
+
+                do{
+                    pass = true;
+                    assign = rand.nextInt(emptyValue) + 1;
+
+                    for (int n : numbers) {
+                        if (assign == n){
+                            pass = false;
+                        }
+                    }
+                } while (!pass);
+
+                for(int k = 0; k < emptyValue; k++){
+                    if(numbers[k] == 0){
+                        numbers[k] = assign;
+                        break;
+                    }
+                }
+
+                //creates square
+                squares[i][j] = new Square(
+                        space + initLeft + (initRight - initLeft) * i,
+                        space + initTop + (initBottom - initTop) * j,
+                        initRight + (initRight - initLeft) * i,
+                        initBottom + (initBottom - initTop) * j,
+                        assign);
+
+                //Log.d("Testing","init " + squares[i][j].getNum());
+            }
+        }
     }
 }
